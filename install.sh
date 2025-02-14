30e824c808ab739dd175d4c553251ac95b6b5c65
HrishabhMittal <hrishabhmittal.hm@gmail.com>
2025-02-14 13:25:51 +0530

    fix install script

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 INSTALL_DIR="/usr/local/bin"
-MAN_DIR="/usr/local/share/man/man1"
-
+MAN_DIR="/usr/share/man/man1"
+mkdir -p $MAN_DIR
 echo "Installing fileman..."
 install -m 755 src/fileman.sh "$INSTALL_DIR/fileman"
 echo "Adding man entry..."
